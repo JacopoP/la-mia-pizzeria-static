eabe1b4a7b5707f6b3c87a57c3b498940090098a
Jac22 <jacopo.prudentino@gmail.com>
2023-04-28 16:23:13 +0200

    Add project files.

---
+++ Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Img { get; set; }
+        public double Prezzo { get; set; }
+
+        public Pizza(string nome, string descrizione, string img, double prezzo)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Img = img;
+            Prezzo = prezzo;
+        }
+    }
+}
